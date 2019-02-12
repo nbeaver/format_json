02919df58e501de1744f35ed84aef7023a7a8e10
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-02-11 22:31:24 -0600

    Use tempfiles instead.

--- format_json.py
+++ format_json.py
@@ -3,21 +3,40 @@
 import sys
 import json
 import argparse
+import tempfile
+import os
 
-def format_json(fp):
-    # TODO: use a proper temporary file instead.
-    # https://backreference.org/2011/01/29/in-place-editing-of-files/
-    try:
-        data = json.load(fp)
-    except ValueError:
-        sys.stderr.write("In file: {}\n".format(fp.name))
-        raise
-    # Jump back to the beginning of the file before overwriting it.
-    fp.seek(0)
-    fp.truncate(0)
-    json.dump(data, fp, ensure_ascii=False, indent=4, separators=(',', ': '), sort_keys=True)
-    fp.write('\n') # add a trailing newline.
-    fp.close()
+# https://backreference.org/2011/01/29/in-place-editing-of-files/
+def format_json_in_place(pathname):
+    dirname = os.path.dirname(pathname)
+    with open(pathname, 'r') as fp:
+        try:
+            data = json.load(fp)
+        except ValueError:
+            sys.stderr.write("In file: {}\n".format(fp.name))
+            raise
+    # Create a temporary file in the same directory.
+    with tempfile.NamedTemporaryFile(dir=directory, delete=False) as tmp_fp:
+        json.dump(
+            data,
+            tmp_fp,
+            ensure_ascii=False,
+            indent=4,
+            separators=(',', ': '),
+            sort_keys=True
+        )
+        tmp_fp.write('\n') # add a trailing newline.
+    # Replace the file atomically.
+    os.replace(tmp_fp.name, pathname)
+
+def writeable_file(pathname):
+    if not os.path.isfile(pathname):
+        raise argparse.ArgumentTypeError(
+            'not an existing file: {}'.format(pathname))
+    if not os.access(pathname, os.W_OK):
+        raise argparse.ArgumentTypeError(
+            'not a writable file: {}'.format(pathname))
+    return pathname
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -25,10 +44,10 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         'files',
-        type=argparse.FileType('r+'),
+        type=writeable_file,
         help='JSON filepaths',
         nargs='+'
     )
     args = parser.parse_args()
     for json_file in args.files:
-        format_json(json_file)
+        format_json_in_place(json_file)
